f9f4ea7268c501829c049593228d8501c4a93536
waffpad <excander@gmail.com>
2020-01-23 14:31:58 +0300

    hw6

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,69 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
+void Engine::Store(context &ctx) {
+    char address;
+    ctx.Low = std::min(&address, StackBottom);
+    ctx.Hight = std::max(StackBottom, &address);
+    auto &buf = std::get<0>(ctx.Stack);
+    auto &size = std::get<1>(ctx.Stack);
+    auto cur_size = ctx.Hight - ctx.Low;
+    if (size < cur_size) {
+        delete[] buf;
+        size = cur_size;
+        buf = new char[size];
+    }
+    memcpy(buf, ctx.Low, cur_size);
+}
 
-void Engine::Restore(context &ctx) {}
+void Engine::Restore(context &ctx) {
+    char address;
+    if ((ctx.Low <= &address) && (ctx.Hight > &address)) {
+        Restore(ctx);
+    }
+    auto &buf = std::get<0>(ctx.Stack);
+    auto &size = std::get<1>(ctx.Stack);
+    memcpy(ctx.Low, buf, size);
+    cur_routine = &ctx;
+    longjmp(ctx.Environment, 1);
+}
 
-void Engine::yield() {}
+void Engine::yield() {
+    if (alive == nullptr) {
+        return;
+    }
+    auto routine = alive;
+    if (routine == cur_routine) {
+        if (routine->next != nullptr) {
+            routine = routine->next;
+        } else {
+            return;
+        }
+    }
 
-void Engine::sched(void *routine_) {}
+    Enter(*routine);
+}
+
+void Engine::sched(void *routine_) {
+    if (routine_ == nullptr) {
+        yield();
+    } else if (routine_ == cur_routine) {
+        return;
+    }
+
+    Enter(*(static_cast<context *>(routine_)));
+}
+
+void Engine::Enter(context &ctx) {
+    if (cur_routine != nullptr && cur_routine != idle_ctx) {
+        if (setjmp(cur_routine->Environment) > 0) {
+            return;
+        }
+        Store(*cur_routine);
+    }
+    cur_routine = &ctx;
+    Restore(ctx);
+}
 
 } // namespace Coroutine
 } // namespace Afina
